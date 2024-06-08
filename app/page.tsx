8cecaffba9121b2286917fbd7b0f555b7f8e4d0c
Coline Phitoyo <colinephitoyo@Colines-Macbook-Air.local>
2024-06-08 21:03:28 +0800

    cleanup backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
 import "bootstrap/dist/css/bootstrap.css";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth";
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (session == null) {
+    return (
+    <div className="text-2xl">a sleeping pup</div>
+    );
+  }
+
   return (
     <div>
-      <h2>(Placeholder) the pup and the happiness bar will be in this page</h2>
+      <h2>pup happiness level</h2>
     </div>
   );
 }
